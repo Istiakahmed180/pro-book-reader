3c1f8e8a379d269d36a1ba30e2783b97707c2a39
Istiakahmed180 <istiakahmed180@gmail.com>
2023-01-18 22:23:57 +0600

    pro-book-reader complete

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./Routes/Routes";
 
 function App() {
-  return <div className="App">Welcome Istiak Ahmed</div>;
+  return (
+    <div className="">
+      <RouterProvider router={router}></RouterProvider>
+    </div>
+  );
 }
 
 export default App;
--- src/components/ErrorPage.js
+++ src/components/ErrorPage.js
@@ -1,15 +1,22 @@
-import React from 'react'
-import Header from './Header'
+import React from "react";
+import { useRouteError } from "react-router-dom";
+import Header from "./Header";
 
 const ErrorPage = () => {
+  let error = useRouteError();
+
   return (
     <>
       <Header />
-      <div className='flex flex-col min-h-[700px] justify-center items-center'>
-        <h1 className='text-4xl'>Ops! An Error Ocurred!</h1>
+      <div className="flex flex-col min-h-[700px] justify-center items-center">
+        <h1 className="text-4xl">Ops! An Error Ocurred!</h1>
+        <p className="text-xl font-semibold text-red-500 my-4">
+          {error.statusText}
+        </p>
+        <p className="text-xl font-semibold text-red-500">{error.data}</p>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default ErrorPage
+export default ErrorPage;
